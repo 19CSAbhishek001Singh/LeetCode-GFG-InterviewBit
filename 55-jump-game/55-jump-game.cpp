32151ed488f8c5601efce19a60015469d38eb1fd
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-01 21:40:53 +0530

    Time: 109 ms (40.62%), Space: 48.4 MB (52.23%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int r=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>r){
+                return false;
+            }
+            r=max(r,nums[i]+i);
+        }
+        return true;
+    }
+};
