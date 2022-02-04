c200f4e563e61f5a33325e062669830adee97ab9
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-04 22:08:43 +0530

    Time: 10 ms (86.36%), Space: 11.2 MB (16.84%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map <int,int> umap;
+        vector<int> res(2);
+        int rem;
+        for(int i=0;i<nums.size() ; i++)
+        {
+            rem=target - nums[i];
+            if(umap.find(rem) != umap.end())
+            {
+                res[0]=umap.at(rem);
+                res[1]=i;
+                return res;
+            }
+            umap.insert({nums[i],i});
+        }
+        return res;
+    }
+};
