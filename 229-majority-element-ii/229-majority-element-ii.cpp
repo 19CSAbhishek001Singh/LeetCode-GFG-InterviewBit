e16c001e1f2f9ef4c20a2fa08744ea0f7f5e8af6
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-05 23:09:19 +0530

    Time: 8 ms (96.57%), Space: 15.9 MB (51.77%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,16 +1,52 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> v;
-        map<int,int> m;
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
+        // vector<int> v;
+        // map<int,int> m;
+        // for(int i=0;i<nums.size();i++){
+        //     m[nums[i]]++;
+        // }
+        // for(auto it:m){
+        //     if(it.second>nums.size()/3){
+        //       v.push_back(it.first);  
+        //     }
+        // }
+        // return v;
+        
+        
+        
+//         in linear time and in O(1) space
+//          boyer moore voting algo
+        
+ int cnt1 = 0, cnt2 = 0, a=0, b=1;
+    for(auto n: nums){
+        if (a==n){
+            cnt1++;
         }
-        for(auto it:m){
-            if(it.second>nums.size()/3){
-              v.push_back(it.first);  
-            }
+        else if (b==n){
+            cnt2++;
         }
-        return v;
+        else if (cnt1==0){
+            a = n;
+            cnt1 = 1;
+        }
+        else if (cnt2 == 0){
+            b = n;
+            cnt2 = 1;
+        }
+        else{
+            cnt1--;
+            cnt2--;
+        }
+    }
+    cnt1 = cnt2 = 0;
+    for(auto n: nums){
+        if (n==a)   cnt1++;
+        else if (n==b)  cnt2++;
+    }
+    vector<int> res;
+    if (cnt1 > nums.size()/3)   res.push_back(a);
+    if (cnt2 > nums.size()/3)   res.push_back(b);
+    return res;
     }
 };
