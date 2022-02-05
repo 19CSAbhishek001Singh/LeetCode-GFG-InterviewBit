c4655781c8bdf7999961938f376a048edc81ab5e
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-05 22:32:00 +0530

    Time: 16 ms (89.39%), Space: 19.6 MB (51.72%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> s;
-        int n=nums.size();
-        for(auto it: nums){
-            s[it]++;
-        }
-        int ans;
-        for(auto it:s){
-            if(it.second>n/2){
-                ans = it.first;
+        // map<int,int> s;
+        // int n=nums.size();
+        // for(auto it: nums){
+        //     s[it]++;
+        // }
+        // int ans;
+        // for(auto it:s){
+        //     if(it.second>n/2){
+        //         ans = it.first;
+        //     }
+        // }
+        // return ans;
+        
+        
+//         in linear time and in O(1) space
+//         moore voting algo
+        
+        int count=0,candidate=0;
+        for(int num : nums){
+            if(count==0) candidate=num;
+            if(num==candidate) count++;
+            else{
+                count--;
             }
         }
-        return ans;
+        return candidate;
+        
     }
 };
