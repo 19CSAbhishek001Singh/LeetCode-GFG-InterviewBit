652277ec038e3c81895beede3d65647059851db7
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-07 21:27:36 +0530

    Time: 1164 ms (5.01%), Space: 41 MB (37.57%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size(); 
-        int currSum = 0  ; 
-        unordered_map<int,int>mp ; 
-        int res = 0 ;  
-        for(int i=0; i<n; ++i){   
-            currSum+=nums[i]  ;                   
-            if(currSum==k)
-            res++ ;                        
-            if(mp.find(currSum-k) != mp.end()){
-                res+=mp[currSum-k]  ;   
-                    }
-             mp[currSum]++ ;
-            }   
-            return res ; 
+        // int n=nums.size(); 
+        // int currSum = 0  ; 
+        // unordered_map<int,int>mp ; 
+        // int res = 0 ;  
+        // for(int i=0; i<n; ++i){   
+        //     currSum+=nums[i]  ;                   
+        //     if(currSum==k)
+        //     res++ ;                        
+        //     if(mp.find(currSum-k) != mp.end()){
+        //         res+=mp[currSum-k]  ;   
+        //             }
+        //      mp[currSum]++ ;
+        //     }   
+        //     return res ; 
         
         
 //         or
         
-      // multiset<int> s;
-      // int result = 0;  
-      // int sum = 0;  
-      // s.emplace(sum);
-      // for(int n: nums) {
-      //   sum += n;
-      //   result += s.count(sum - k);  
-      //   s.emplace(sum);  
-      // }
-      // return result;  
+      multiset<int> s;
+      int result = 0;  
+      int sum = 0;  
+      s.emplace(sum);
+      for(int n: nums) {
+        sum += n;
+        result += s.count(sum - k);  
+        s.emplace(sum);  
+      }
+      return result;  
     }
 };
