405ca1fda96728b1acc511a48e0d6cc07340523e
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-13 23:37:27 +0530

    Time: 17 ms (34.44%), Space: 17.9 MB (5.84%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,26 +3,43 @@ public:
     int trap(vector<int>& height) {
         
         // with O(1) space 
-        int n=height.size();
-        int left=0,right=n-1;
-        int res=0;
-        int maxleft=0,maxright=0;
-        while(left<=right){
+//         int n=height.size();
+//         int left=0,right=n-1;
+//         int res=0;
+//         int maxleft=0,maxright=0;
+//         while(left<=right){
             
-            if(height[left]<=height[right]){
+//             if(height[left]<=height[right]){
                 
-            if(height[left]>=maxleft) maxleft=height[left];
-            else
-                res+=maxleft-height[left];
-                
-            left++;
-            }
-            else{
-                  if(height[right]>=maxright) maxright=height[right];
-                    else
-                    res+=maxright-height[right];
-                 right--;
-            }
+//             if(height[left]>=maxleft) maxleft=height[left];
+//             else res+=maxleft-height[left];
+//             left++;
+//             }
+//             else{
+//                   if(height[right]>=maxright) maxright=height[right];
+//                     else res+=maxright-height[right];
+//                  right--;
+//             }
+//         }
+//         return res;
+        
+        
+        
+        // O(n) space and O(n) time
+        int res=0;
+        int n=height.size();
+        int lmax[n],rmax[n];
+        lmax[0]=height[0];
+        for(int i=1;i<n;i++){
+            lmax[i]=max(height[i],lmax[i-1]);
+        }
+        rmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rmax[i]=max(height[i],rmax[i+1]);
+        }
+        
+        for(int i=1;i<n-1;i++){
+            res+=min(lmax[i],rmax[i])-height[i];
         }
         return res;
         
