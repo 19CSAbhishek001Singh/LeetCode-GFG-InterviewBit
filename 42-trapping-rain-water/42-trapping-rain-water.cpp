c2b9c7799b0284c67d878837cd1697aa0445ab0c
Abhishek <abhisheksinghcsanand@gmail.com>
2022-02-13 23:38:48 +0530

    Time: 23 ms (16.44%), Space: 17.7 MB (5.84%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,30 +3,31 @@ public:
     int trap(vector<int>& height) {
         
         // with O(1) space 
-//         int n=height.size();
-//         int left=0,right=n-1;
-//         int res=0;
-//         int maxleft=0,maxright=0;
-//         while(left<=right){
+        int n=height.size();
+        int left=0,right=n-1;
+        int res=0;
+        int maxleft=0,maxright=0;
+        while(left<=right){
             
-//             if(height[left]<=height[right]){
+            if(height[left]<=height[right]){
                 
-//             if(height[left]>=maxleft) maxleft=height[left];
-//             else res+=maxleft-height[left];
-//             left++;
-//             }
-//             else{
-//                   if(height[right]>=maxright) maxright=height[right];
-//                     else res+=maxright-height[right];
-//                  right--;
-//             }
-//         }
-//         return res;
+            if(height[left]>=maxleft) maxleft=height[left];
+            else res+=maxleft-height[left];
+            left++;
+            }
+            else{
+                  if(height[right]>=maxright) maxright=height[right];
+                    else res+=maxright-height[right];
+                 right--;
+            }
+        }
+        return res;
         
         
         
         // O(n) space and O(n) time
-        int res=0;
+       /* 
+         int res=0;
         int n=height.size();
         int lmax[n],rmax[n];
         lmax[0]=height[0];
@@ -42,22 +43,23 @@ public:
             res+=min(lmax[i],rmax[i])-height[i];
         }
         return res;
+        */
         
+
         
-        
-        
-// int sum=0,max_left =0,max_right =0;
-// for(int i=1;i<height.size();i++){
-// max_left =*max_element(height.begin() ,height.begin()+i);
-// max_right= *max_element(height.begin()+i,height.end());
-// int ans = min(max_left,max_right)-height.at(i);
-// if(ans<1){
-// ans =0 ;
-// }
-// sum= sum+ans;
-// }
-// return sum;
-        
+ /*     
+int sum=0,max_left =0,max_right =0;
+for(int i=1;i<height.size();i++){
+max_left =*max_element(height.begin() ,height.begin()+i);
+max_right= *max_element(height.begin()+i,height.end());
+int ans = min(max_left,max_right)-height.at(i);
+if(ans<1){
+ans =0 ;
+}
+sum= sum+ans;
+}
+return sum;
+*/
         
     }
 };
