1a16b7b8f6b588cbd8f4b17a3ce85cc2e479543d
Abhishek <abhisheksinghcsanand@gmail.com>
2022-03-17 20:30:48 +0530

    Time: 43 ms (21.59%), Space: 6.2 MB (7.82%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        
     // 1162261467 is 3^19,  3^20 is bigger than int  
-    return ( n>0 &&  1162261467%n==0);
+   // return ( n>0 &&  1162261467%n==0);
+        
+    if(n==0) return false;    
+    if(ceil(log10(n) / log10(3))==floor(log10(n) / log10(3))){
+         return true;
+     }
+     return false;
     }
 };
