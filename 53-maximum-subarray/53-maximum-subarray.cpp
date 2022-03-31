9b76823be4cd37f91b24f6a641b1d02eca6bc779
Abhishek <abhisheksinghcsanand@gmail.com>
2022-03-31 05:57:57 +0530

    Update 53-maximum-subarray.cpp

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& arr) {
-        if(arr.size()<0) return 0;
-         int curr=arr[0];
-        int res=arr[0];
-        int n=arr.size();
-        for(int i=1;i<n;i++){
-            if(curr<0){
-                curr=0;
-            }
-            curr=curr+arr[i];
-            if(curr>res){
-                res=curr;
-            }
-        }
-        return res;
+    int maxSubArray(vector<int>& nums) {
+        
+ // subarray vector to store subarray       
+ //vector<int> subarray;
+ int maxi = nums[0], sum = 0;
+  //int s = 0;
+  for (int i = 0; i < nums.size(); i++) {
+    sum += nums[i];
+    if (sum > maxi) {
+     // subarray.clear();
+      maxi = sum;
+    //  subarray.push_back(s);
+    //  subarray.push_back(i);
+    }
+    if (sum < 0) {
+      sum = 0;
+     // s = i + 1;
+    }
+  }
+  //   for (int i = subarray[0]; i <= subarray[1]; i++) {
+  //   cout << nums[i] << " ";
+  // }
+  //       cout<<endl;
+  return maxi;
     }
 };
+
+// solve problem 1749 too
