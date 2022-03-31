7e0cc7cc4145bf5dd686083e6cf6f58058f02b8a
Abhishek <abhisheksinghcsanand@gmail.com>
2022-03-31 06:17:31 +0530

    Update 121-best-time-to-buy-and-sell-stock.cpp

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-          int maxPro = 0;
-        int minPrice = INT_MAX;
-        for(int i = 0; i < prices.size(); i++){
-            minPrice = min(minPrice, prices[i]);
-            maxPro = max(maxPro, prices[i] - minPrice);
+        int maxPro=0;
+        int minPrice=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            minPrice=min(minPrice,prices[i]);
+            maxPro=max(maxPro,prices[i]-minPrice);
         }
         return maxPro;
     }
 };
+// also solve buy and sell stoc 2 and 3 problem no 122 and 123
