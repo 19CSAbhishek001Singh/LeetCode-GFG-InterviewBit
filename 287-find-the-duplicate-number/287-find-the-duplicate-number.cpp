308d0eed5a3bb4f0a30a88ce5cece6cf44dc735e
Abhishek <abhisheksinghcsanand@gmail.com>
2022-03-31 11:55:31 +0530

    Update 287-find-the-duplicate-number.cpp

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-       int slow=nums[0];
-       int fast=nums[0];
-       do{
-           slow=nums[slow];
-           fast=nums[nums[fast]];
-       } while(slow!=fast);
+        int slow=nums[0];
+        int fast= nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
         fast=nums[0];
-        while(fast!=slow){
+        while(slow!=fast){
             slow=nums[slow];
             fast=nums[fast];
         }
         return slow;
     }
 };
+
+// also solve this one
+//https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1#
