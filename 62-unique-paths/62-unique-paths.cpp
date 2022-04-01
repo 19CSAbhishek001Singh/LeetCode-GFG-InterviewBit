5c2190685e0bca72cf47f6aa8d63038efb760ca7
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-01 10:38:22 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.09%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
    // using dp
-    int countPaths(int i,int j,int n,int m,vector<vector<int>> &dp){
-        if(i==(n-1)&&j==(m-1)) return 1;
-        if(i>=n||j>=m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-     else return dp[i][j]=countPaths(i+1,j,n,m,dp)+countPaths(i,j+1,n,m,dp);
-    }
+    // int countPaths(int i,int j,int n,int m,vector<vector<int>> &dp){
+    //     if(i==(n-1)&&j==(m-1)) return 1;
+    //     if(i>=n||j>=m) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //  else return dp[i][j]=countPaths(i+1,j,n,m,dp)+countPaths(i,j+1,n,m,dp);
+    // }
     
     int uniquePaths(int m, int n) {
         
-         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-         int num=countPaths(0,0,m,n,dp);
-         if(m==1&&n==1)
-         return num;
+//          vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+//          int num=countPaths(0,0,m,n,dp);
+//          if(m==1&&n==1)
+//          return num;
+//         return dp[0][0];
         
-        return dp[0][0];
         
-        
-         /* Combinatorics Solution 
+         // Combinatorics Solution 
         // how much steps we need to do
             int N = n + m - 2;
          // number of steps that need to go down
@@ -31,9 +30,8 @@ public:
             for (int i = 1; i <= k; i++)
                 res = res * (N - k + i) / i;
             return (int)res;
-        */
-        
-        
-
     }
 };
+// also solve
+// https://leetcode.com/problems/unique-paths-ii/
+// https://leetcode.com/problems/minimum-path-sum/
