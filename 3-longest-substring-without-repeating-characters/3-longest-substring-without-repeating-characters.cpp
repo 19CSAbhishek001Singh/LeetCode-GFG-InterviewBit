ea2b3e9a1344c61253e9c31e079de49efb4586b4
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-10 13:44:19 +0530

    Time: 4 ms (96.13%), Space: 8.3 MB (57.71%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -6,7 +6,12 @@ public:
         int n=s.size();
         int len=0;
         while(right<n){
-           if(mpp[s[right]]!=-1) left=max(mpp[s[right]]+1,left);
+            // Checking if we have already seen the element or not
+           if(mpp[s[right]]!=-1)
+            // If we have seen the number, move the left
+            // pointer to position after the right occurrence
+            left=max(mpp[s[right]]+1,left);
+            // Updating the last seen value of the character
             mpp[s[right]]=right;
             len=max(len,right-left+1);
             right++;
