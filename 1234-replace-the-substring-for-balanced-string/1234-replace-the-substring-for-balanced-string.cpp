3f2d550fb186dc2e8c1c92e2ee874aa4593243c3
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-13 06:11:33 +0530

    Time: 61 ms (51.27%), Space: 7.9 MB (49.58%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int balancedString(string s) {
+       int n=s.size();
+       unordered_map<char,int>count;    // Used to count the repeatition of a character
+       for(int i=0;i<n;i++) {
+           count[s[i]]++;               // Increment count
+       }
+     int l=0,i=0,window=n;// The window we need to replace ,and we want
+                         //  to search for a minimum window there
+       int m=n/4;    
+       while(i<n){
+         count[s[i]]--;
+        while(count['Q']<=m && count['W']<=m && count['E']<=m && count['R']<=m){  
+               count[s[l]]++;
+               window=min(window,i-l+1);    // Update window size 
+               l++;                         // remove l'th character from window
+               if(l>=n)                     // If it reaches to n simply back of
+                   break;
+           }
+           i++;
+       }
+       return window;
+    }
+};
