bfe1081611f63adb54af77807a0799e3b2104c6b
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-19 15:32:02 +0530

    Time: 3 ms (85.51%), Space: 11.2 MB (29.81%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+  int low = 0, high = nums.size() - 1; 
+  while (low <= high) { 
+    int mid = (low + high) >> 1; 
+    if (nums[mid] == target)
+      return mid;
+    if (nums[low] <= nums[mid]) { 
+      if (nums[low] <= target && nums[mid] >= target)
+        high = mid - 1; 
+      else
+        low = mid + 1; 
+    } else { 
+      if (nums[mid] <= target && target <= nums[high])
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+  }
+  return -1; 
+    }
+};
+  // also do
+ // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
