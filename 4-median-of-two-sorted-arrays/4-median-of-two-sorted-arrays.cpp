f62a39b621771364228db86f53f1cf6b317d287a
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-20 11:56:36 +0530

    Update 4-median-of-two-sorted-arrays.cpp

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,12 +1,41 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-      int m = nums1.size(),n=nums2.size();
-        vector<int> v(m+n);
-        merge(nums1.begin(),nums1.end(),nums2.begin(),nums2.end(),v.begin());
-        if((m+n)%2!=0)
-            return (double)v[(m+n)/2];
-        else
-            return (double)(v[((m+n)-1)/2]+v[(m+n)/2])/2.0;
+         if(nums2.size()<nums1.size())
+    //ensuring that binary search happens on minimum size array
+        return findMedianSortedArrays(nums2,nums1);
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int low=0,high=n1;
+       while(low<=high) {
+        int cut1 = (low+high)>>1;
+        int cut2 = (n1+n2+1)/2 - cut1;
+         
+        int left1 = (cut1 == 0) ? INT_MIN : nums1[cut1-1];
+        int left2 = (cut2 == 0) ? INT_MIN : nums2[cut2-1];
+        int right1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+        int right2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+        
+        if(left1<=right2 && left2<=right1) {
+            if((n1+n2)%2 != 0)
+                return max(left1,left2);
+            else 
+                return (max(left1,left2)+min(right1,right2))/2.0;
+        }
+        else if(left1>right2) high = cut1-1;
+        else low = cut1+1;
+    }
+    return 0.0;
     }
 };
+
+// O(logm+n)
+
+     /*
+    vector<int> mergedSortedArray(nums1.size()+nums2.size());
+    merge(nums1.begin(),nums1.end(),nums2.begin(),nums2.end(),mergedSortedArray.begin());
+    if(mergedSortedArray.size() & 1){
+        return mergedSortedArray[(mergedSortedArray.size())*0.5];
+    }
+     return ((mergedSortedArray[((mergedSortedArray.size())*0.5)-1] +          mergedSortedArray[((mergedSortedArray.size())*0.5)])*0.5);
+     */
