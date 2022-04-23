647ba4cebce9505f807c27195535d51eba55477d
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-23 07:33:42 +0530

    Update 46-permutations.cpp

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
+     void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
+      if (index == nums.size()) {
+        ans.push_back(nums);
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        swap(nums[index], nums[i]);
+        recurPermute(index + 1, nums, ans);
+        swap(nums[index], nums[i]);
+      }
+    }
     vector<vector<int>> permute(vector<int>& a) {
-     vector<vector<int>> ans;
+     vector < vector < int >> ans;
+      recurPermute(0, a, ans);
+      return ans;
+    }
+};
+    /*
+    vector<vector<int>> ans;
      sort(a.begin(),a.end());
     do{
      ans.push_back(a);
@@ -9,4 +26,4 @@ public:
      while(next_permutation(a.begin(),a.end()));
        return ans;  
     }
-};
+    */
