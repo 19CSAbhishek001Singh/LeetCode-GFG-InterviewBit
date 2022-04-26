d384f306f9f288199aa937740ed25a7471631005
Abhishek <abhisheksinghcsanand@gmail.com>
2022-04-26 06:41:05 +0530

    Time: 162 ms (22.02%), Space: 5.8 MB (99.08%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string ans;
+        for(int i=1;i<=n;i++){
+            ans+=(i+'0');
+        }
+        for(int i=0;i<k-1;i++){
+            next_permutation(ans.begin(),ans.end());
+        }
+        return ans;
+    }
+};
