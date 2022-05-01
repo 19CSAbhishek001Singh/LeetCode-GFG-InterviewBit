b28ef778d00101eda47bd8bd1726372bf345ac85
Abhishek <abhisheksinghcsanand@gmail.com>
2022-05-01 14:03:07 +0530

    Time: 169 ms (25.00%), Space: 35.9 MB (50.00%) - LeetHub

---
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+      long long int count(string st,char ch){ 
+        long long int len=st.length();
+        long long int res=0;
+        long long int cnt=0;
+        for(long int i=0;i<len;i++){
+            if(st[i]==ch){
+                res+=((cnt)*(cnt+1))/2;
+                cnt=0;
+            }
+            else
+                cnt++;
+        }
+        res+=((cnt)*(cnt+1))/2;
+        return (len*(len+1))/2 - res;
+    }
+    long long appealSum(string st) {
+ 
+        long long int total_cnt=0;
+        for(char ch='a';ch<='z';ch++)
+            total_cnt+=count(st,ch);
+ 
+        return total_cnt;
+    }
+};
