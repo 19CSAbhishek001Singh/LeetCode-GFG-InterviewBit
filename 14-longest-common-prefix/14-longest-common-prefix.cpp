2330e2113a8a65f8e65acde528dba50c7f2eaafb
Abhishek <abhisheksinghcsanand@gmail.com>
2022-05-07 07:54:29 +0530

    Time: 13 ms (10.44%), Space: 9.6 MB (14.23%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,7 +1,66 @@
+struct Node{
+  Node *links[26];
+  bool isEnd=false;
+};
+class Trie{
+    private:
+    Node *root;
+    public:
+    Trie(){
+    root=new Node();
+    }
+    void insert(string s){
+        Node* node = root;
+        for(char &ch:s){
+            if(!node->links[ch-'a'])
+            node->links[ch-'a']= new Node();
+            node=node->links[ch-'a'];
+        }
+        node->isEnd=true;
+    }
+    void search(string s){
+        Node* node = root;
+        for(char &ch:s){
+            if(!node->links[ch-'a']){
+                node->isEnd = true;
+                break;
+            }
+            node=node->links[ch-'a'];
+        }
+        node->isEnd = true;
+    }
+    string prefix(string s){
+        Node* node = root;
+        string res = "";
+        for(char &ch:s){
+            if(!node->isEnd){
+                res+=ch;
+            }else{
+                break;
+            }
+            node=node->links[ch-'a'];
+        }
+        return res;
+    }
+};
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& str) {
-        int n = str.size();
+    string longestCommonPrefix(vector<string>& s) {
+        int n = s.size();
+        if(n==0)
+            return "";
+        if(n==1)
+            return s[0];
+        Trie* t = new Trie();
+        t->insert(s[0]);
+        for(int i=1;i<n;i++)
+            t->search(s[i]);
+        
+        return t->prefix(s[0]);
+    }
+};
+    /*
+     int n = str.size();
         if(n==0) return "";
         
         string ans  = "";
@@ -20,4 +79,4 @@ public:
         
         return ans;
     }
-};
+    */
