0d0b816b3022bc73b4f22867bfc414248069ffa1
Abhishek <abhisheksinghcsanand@gmail.com>
2022-05-11 12:28:30 +0530

    Time: 264 ms (33.02%), Space: 165.9 MB (18.26%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,50 +1,87 @@
-struct TrieNode {
-    TrieNode *children[26] = {NULL};
-    bool endOfWord = false;
-};
-class Solution {
-public:
-    void insertWord(TrieNode *trie, string &word) {
-        for (char &c : word) {
-            int index = c - 'a';
-            if (!trie->children[index])
-                trie->children[index] = new TrieNode;
-            trie = trie->children[index];
-        }
-        trie->endOfWord = true;
-    }
-    vector<string> searchWord(TrieNode *trie, string &prefix) {
-        vector<string> result;
-        for (char &c : prefix) {
-            int index = c - 'a';
-            if (!trie->children[index])
-                return {};
-            trie = trie->children[index];
-        }
-        dfs(trie, prefix, result);
-        return result;
-    }
-    void dfs(TrieNode *trie, string pre, vector<string> &result) {
-        if (result.size() == 3)
-            return;
-        if (trie->endOfWord)
-            result.push_back(pre);
-        
-        for (int i = 0; i < 26; i++) {
-            if (trie->children[i])
-                dfs(trie->children[i], pre + (char)(i + 'a'), result);
-        }
-    }
-    vector<vector<string>> suggestedProducts(vector<string>& products, string search) {
-        TrieNode *trie = new TrieNode;
-        for (string &prod : products)
-            insertWord(trie, prod);
-        vector<vector<string>> result;
-        string prefix;
-        for (char &c : search) {
-            prefix += c;
-            result.push_back(searchWord(trie, prefix));
-        }
-        return result;
-    }
-};
+struct Node{
+       Node *links[26];
+       vector<string> v;
+       
+       bool containsKey(char ch){
+           return links[ch-'a'] != nullptr;
+       }
+       
+       Node* getKey(char ch){
+           return links[ch-'a'];
+       }
+       
+       void put(char ch, Node *node){
+           links[ch-'a'] = node;
+       }
+       
+       void insertString(string str){
+           v.push_back(str);
+       }
+       
+       vector<string> getV(){
+           return (v);
+       }
+   };
+   
+   class trie{
+       private:
+       Node *root;
+       
+       public:
+       trie(){
+           root = new Node();
+       }
+       
+       void insert(string word){
+           Node *node = root;
+           for(int i = 0; i < word.length(); i++){
+               if(!node->containsKey(word[i])){
+                   node->put(word[i],new Node());
+               }
+               node = node->getKey(word[i]);
+               node->insertString(word);
+           }
+       }
+       
+       vector<string> getStrings(string word){
+           Node *node = root;
+           for(int i = 0; i < word.length(); i++){
+               if(!node->containsKey(word[i])){
+                   return vector<string>{};
+               }
+               node = node->getKey(word[i]);
+           }
+           return node->getV();
+       }
+   };
+   class Solution{
+     public:
+   vector<vector<string>> suggestedProducts(vector<string>& contact, string s){
+       trie tri;
+       set<string> st;
+       int n=contact.size();
+       for(int i = 0; i < n; i++){
+           st.insert(contact[i]);
+       }
+       
+       set<string>::iterator iter;
+       for(iter = st.begin(); iter != st.end(); iter++){
+           tri.insert(*iter);
+       }
+       
+       vector<vector<string>> ans;
+       string temp{};
+       for(int i = 0; i < s.length(); i++){
+           temp += s[i];
+           vector<string> v = tri.getStrings(temp);
+            if(v.size()>3){
+                vector<string> s(v.begin(),v.begin()+3);
+                 ans.push_back(s);
+            }
+           else
+           ans.push_back(v);
+       }
+       
+       return ans;
+   }
+   };
