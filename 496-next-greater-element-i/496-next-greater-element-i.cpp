0ed4eaf1f369f1fb256cfc1ab1f75c1c1fec1e44
Abhishek <abhisheksinghcsanand@gmail.com>
2022-05-17 14:36:04 +0530

    Time: 14 ms (32.94%), Space: 9 MB (30.31%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -5,7 +5,7 @@ public:
         stack<int>st;
         int n=nums2.size();
         for(int i=n-1;i>=0;i--){
-            while(!st.empty()&&st.top()<nums2[i]){
+            while(!st.empty()&&st.top()<=nums2[i]){
                 st.pop();
             }
             int res=st.empty()?-1:st.top();
