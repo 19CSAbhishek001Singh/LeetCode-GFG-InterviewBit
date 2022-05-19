0d170e3595b5081ddbfb9f48f3e8a31aca27c196
Abhishek <abhisheksinghcsanand@gmail.com>
2022-05-19 07:47:19 +0530

    Time: 32 ms (45.86%), Space: 16.3 MB (69.25%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,4 +1,51 @@
 class MinStack {
+  stack < long long > st;
+  long long mini;
+  public:
+    MinStack() {
+      while (st.empty() == false) st.pop();
+      mini = INT_MAX;
+    }
+
+  void push(int value) {
+    long long val = 1LL*value;
+    if (st.empty()) {
+      mini = val;
+      st.push(val);
+    } else {
+      if (val < mini) {
+        st.push(2 * val * 1LL - mini);
+        mini = val;
+      } else {
+        st.push(val);
+      }
+    }
+  }
+
+  void pop() {
+    if (st.empty()) return;
+    long long el = st.top();
+    st.pop();
+
+    if (el < mini) {
+      mini = 2 * mini - el;
+    }
+  }
+
+  int top() {
+    if (st.empty()) return -1;
+
+    long long el = st.top();
+    if (el < mini) return mini;
+    return el;
+  }
+
+  int getMin() {
+    return mini;
+  }
+};
+/*
+class MinStack {
   stack < pair < int, int >> st;
   public:
     void push(int x) {
@@ -23,6 +70,7 @@ class MinStack {
     return st.top().second;
   }
 };
+*/
 
 /**
  * Your MinStack object will be instantiated and called as such:
