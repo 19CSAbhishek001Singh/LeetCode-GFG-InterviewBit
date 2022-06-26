5f85024572e6887e08c9c220b95312f7a1cb80b0
Abhishek <abhisheksinghcsanand@gmail.com>
2022-06-26 12:15:00 +0530

    Time: 17 ms (75.00%), Space: 5.9 MB (75.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     #define mod 1000000007
+// we have n*2 plots n plot on each side so lets take one side of street if answer to that is x
+//  then for n*2 will be x*x
+// try the test cases for few examples
+// for n=1 we have 2*2
+// for n=2 we have 3*3
+// for n=3 we have 5*5
+// see a fibonnaci pattern here 2,3,5....
     int countHousePlacements(int n) {
-         long long y=1,z=1,w=2;
-            for(int x=2;x<=n;x++){
+         long long x=1,y=1,z=2;
+            for(int i=2;i<=n;i++){
+                x=y;
                 y=z;
-                z=w;
-                w=(y+z)%mod;
+                z=(x+y)%mod;
             }
-        return (w*w)%mod;
+        return (z*z)%mod;
     }
 };
