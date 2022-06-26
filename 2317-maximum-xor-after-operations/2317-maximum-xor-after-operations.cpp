64752d238f33a31e6ae803981d602c0b68f8ec44
Abhishek <abhisheksinghcsanand@gmail.com>
2022-06-26 13:11:07 +0530

    Time: 258 ms (50.00%), Space: 48 MB (100.00%) - LeetHub

--- 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
+++ 2317-maximum-xor-after-operations/2317-maximum-xor-after-operations.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
         vector<int>v(32);
+		// go to  nums[i] then go to binary representation o nums[i] check for ith bit set 
+		// if set set value of v[i] to 1
         for(int n:nums){
          for(int i=0;i<32;i++){
                 if(n&(1<<i))
