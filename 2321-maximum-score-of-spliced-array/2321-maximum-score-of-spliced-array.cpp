ef8549b5044bc2fe16387c7a23a8451182993179
Abhishek <abhisheksinghcsanand@gmail.com>
2022-06-26 12:41:32 +0530

    Time: 225 ms (66.67%), Space: 86.2 MB (33.33%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
+	// find the sum of num1 and num2
         long long sum1 = accumulate(nums1.begin(), nums1.end(), 0);
         long long sum2 = accumulate(nums2.begin(), nums2.end(), 0);
         vector<int> v(nums1.size(),0);
         vector<int> v1(nums1.size(),0);
- // Making difference
+ // store difference of the arrays num1-num2 and num2-num1 in these vectors above
         for(int i = 0; i < nums1.size(); i++){
             v[i] = nums2[i] - nums1[i];
         }
@@ -25,7 +26,7 @@ public:
             temp = max(v1[i], temp+v1[i]);
             ans1 = max(temp, ans1);
         }
-        
+        // answer will be sum+max sum subarray
         return max(sum1 + ans, sum2 + ans1);
     }
 };
