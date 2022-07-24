f2e561721befa9e88a9b5a34def2f55a513db1dc
Abhishek <abhisheksinghcsanand@gmail.com>
2022-07-24 10:29:31 +0530

    Time: 189 ms (66.67%), Space: 31.5 MB (33.33%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,31 +1,33 @@
-// class Solution {
-// public:
-//     int equalPairs(vector<vector<int>>& grid) {
-//         multiset<vector<int>> row;
-//         multiset<vector<int>> col;
-//         vector<int> v;
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        multiset<vector<int>> row;
+        multiset<vector<int>> col;
+        vector<int> v;
         
-//         for(int i=0;i<grid.size();i++){
-//          row.insert(grid[i]);
-//         }
+        for(int i=0;i<grid.size();i++){
+         row.insert(grid[i]);
+        }
         
-//        for(int i=0;i<grid.size();i++){
-//            for(int j=0;j<grid[0].size();j++){
-//                v.push_back(grid[j][i]);
-//            }
-//            col.insert(v);
-//            v.clear();
-//        }
+       for(int i=0;i<grid.size();i++){
+           for(int j=0;j<grid[0].size();j++){
+               v.push_back(grid[j][i]);
+           }
+           col.insert(v);
+           v.clear();
+       }
 
-//         int cnt=0;
-//         for(auto x:row){
-//             if(col.find(x)!=col.end()){
-//                 cnt+=col.count(x);
-//             }
-//         }
-//         return cnt;
-//     }
-// };
+        int cnt=0;
+        for(auto x:row){
+            if(col.find(x)!=col.end()){
+                cnt+=col.count(x);
+            }
+        }
+        return cnt;
+    }
+};
+
+/*
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
@@ -45,3 +47,4 @@ public:
         return ans;
     }
 };
+*/
