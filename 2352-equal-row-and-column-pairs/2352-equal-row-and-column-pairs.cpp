ce4a19f02c2f786ff87e176fb0dcc48c38026d99
Abhishek <abhisheksinghcsanand@gmail.com>
2022-07-24 10:28:57 +0530

    Time: 199 ms (66.67%), Space: 32.7 MB (33.33%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,28 +1,47 @@
+// class Solution {
+// public:
+//     int equalPairs(vector<vector<int>>& grid) {
+//         multiset<vector<int>> row;
+//         multiset<vector<int>> col;
+//         vector<int> v;
+        
+//         for(int i=0;i<grid.size();i++){
+//          row.insert(grid[i]);
+//         }
+        
+//        for(int i=0;i<grid.size();i++){
+//            for(int j=0;j<grid[0].size();j++){
+//                v.push_back(grid[j][i]);
+//            }
+//            col.insert(v);
+//            v.clear();
+//        }
+
+//         int cnt=0;
+//         for(auto x:row){
+//             if(col.find(x)!=col.end()){
+//                 cnt+=col.count(x);
+//             }
+//         }
+//         return cnt;
+//     }
+// };
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        multiset<vector<int>> row;
-        multiset<vector<int>> col;
-        vector<int> v;
+        int ans = 0;
+        map<vector<int>, int> mp; // store rows of grid and count frequency
+		
+        for (int i = 0; i < grid.size(); i++)
+            mp[grid[i]]++;
         
-        for(int i=0;i<grid.size();i++){
-         row.insert(grid[i]);
+        for (int i = 0; i < grid.size(); i++) {
+            vector<int> v;
+            for (int j = 0; j < grid[0].size(); j++)
+                v.push_back(grid[j][i]);
+        // add the number of times that col appeared as row in map (frequency)
+            ans += mp[v];
         }
-        
-       for(int i=0;i<grid.size();i++){
-           for(int j=0;j<grid[0].size();j++){
-               v.push_back(grid[j][i]);
-           }
-           col.insert(v);
-           v.clear();
-       }
-
-        int cnt=0;
-        for(auto x:row){
-            if(col.find(x)!=col.end()){
-                cnt+=col.count(x);
-            }
-        }
-        return cnt;
+        return ans;
     }
 };
