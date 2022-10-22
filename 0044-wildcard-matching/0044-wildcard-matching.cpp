eff26d6bb969eeb26c021e1a73a4777d963c62ba
Abhishek <abhisheksinghcsanand@gmail.com>
2022-10-22 18:05:04 +0530

    Time: 83 ms (66.88%), Space: 27.2 MB (42.61%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-	bool f(int i,int j,string& p,string& s,vector<vector<int>>& dp){
+	bool rec(int i,int j,string& p,string& s,vector<vector<int>>& dp){
 		if(i<0 && j<0) return true;
 		if(i<0 && j>=0) return false;
 		if(j<0 && i>=0){
@@ -9,18 +9,22 @@ public:
 			}
 			return true;
 		}
+        
 		if(dp[i][j]!=-1) return dp[i][j];
-//      Match 
-		if(p[i]==s[j] || p[i]=='?') return dp[i][j]=f(i-1,j-1,p,s,dp);
-		if(p[i]=='*') return dp[i][j]=f(i-1,j,p,s,dp) || f(i,j-1,p,s,dp);
-//      NotMatch
+        
+		if(p[i]==s[j] || p[i]=='?') 
+            return dp[i][j]=rec(i-1,j-1,p,s,dp);
+        
+		if(p[i]=='*') 
+            return dp[i][j]=rec(i-1,j,p,s,dp) || rec(i,j-1,p,s,dp);
+        
 		return dp[i][j]=false;
 	}
 
 	bool isMatch(string s, string p) {
 		int n=p.size();
 		int m=s.size();
-		vector<vector<int>> dp(n,vector<int>(m,-1));
-		return f(n-1,m-1,p,s,dp);
+		vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+		return rec(n-1,m-1,p,s,dp);
 	}
 };
