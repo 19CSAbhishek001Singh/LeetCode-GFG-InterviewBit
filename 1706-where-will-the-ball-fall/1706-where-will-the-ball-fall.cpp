8ce5ece4bb7fb5084b84ffd9c0cdc0da6b12cc9c
Abhishek <abhisheksinghcsanand@gmail.com>
2022-11-01 19:09:03 +0530

    Time: 66 ms (21.09%), Space: 13.4 MB (19.11%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
- int m,n;
-    int solve(vector<vector<int>>&grid,vector<vector<int>>&dp,int i,int j){
-        if(i==m)
-            return j;
-        if(dp[i][j]!=101)
-            return dp[i][j];
-        if(j==0 and grid[i][j]==-1)
-            return dp[i][j] = -1;
-        if(j==n-1 and grid[i][j]==1)
-            return dp[i][j] = -1;
-        if(grid[i][j]==1)
-        {
-            if(grid[i][j+1]==-1)
-                return dp[i][j] = -1;
-            return dp[i][j] = solve(grid,dp,i+1,j+1);
-        }
-        if(grid[i][j-1]==1)
-            return dp[i][j] = -1;
-        return dp[i][j] = solve(grid,dp,i+1,j-1);
+    int dp[105][105];
+    int n,m;
+    bool isValid(int x, int y){
+        return (x>=0 && x<n && y>=0 && y<m);
     }
+    int rec(vector<vector<int>>& grid , int r, int c){
+        if(r==n) return c;
+        if(isValid(r,c)){
+            if(grid[r][c]==1 && isValid(r,c+1) && grid[r][c+1]==1){
+                if(dp[r+1][c+1]!=-2) return dp[r+1][c+1];
+                else{
+                    return dp[r+1][c+1] = rec(grid,r+1,c+1);
+                }
+            } 
+        if(grid[r][c]==-1 && isValid(r,c-1) && grid[r][c-1]==-1){
+            if(dp[r+1][c-1]!=-2) return dp[r+1][c-1];
+            else {
+             return dp[r+1][c-1] = rec(grid,r+1,c-1);
+                }
+            } 
+        }
+        return -1;
+    }
+    
     vector<int> findBall(vector<vector<int>>& grid) {
-        m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,101));
-        for(int i = 0;i<n;i++)
-            solve(grid,dp,0,i);
-        return dp[0];
+        n=grid.size();
+        m=grid[0].size();
+        for(int i=0;i<=101;i++){
+            for(int j=0;j<=101;j++) 
+                dp[i][j]=-2;
+        }
+        vector<int>ans(m);
+        for(int i=0;i<m;i++){
+            ans[i]=rec(grid,0,i);
+        }
+        return ans;
     }
 };
