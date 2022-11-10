6c12ad518a329f450c327fed939dbf701f0b951a
Abhishek <abhisheksinghcsanand@gmail.com>
2022-11-10 10:20:21 +0530

    Time: 3 ms (68.51%), Space: 6.1 MB (47.56%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
-
+        // return __builtin_popcount(n);
+        int cnt=0;
+        while(n){
+            if(n&1) cnt++;
+            n>>=1;
+        }
+        return cnt;
     }
 };
