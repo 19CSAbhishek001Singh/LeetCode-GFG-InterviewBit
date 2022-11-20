6a775366431b4d5810fbd6ba2290487961ef574e
Abhishek <abhisheksinghcsanand@gmail.com>
2022-11-20 08:47:16 +0530

    Time: 6 ms (95.99%), Space: 8.1 MB (43.51%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int calculate(string s) {
+         int sum=0;
+        int sign=1;
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            
+            if(isdigit(s[i])){
+                int val=0;
+                while(i<s.size()&&isdigit(s[i])){
+                    val=val*10+(s[i]-'0');
+                    i++;
+                }
+                i--;
+                val=val*sign;
+                sign=1;
+                sum+=val;
+            }
+            
+            else if(s[i]=='('){
+                st.push(sum);
+                st.push(sign);
+                sum=0;
+                sign=1;
+            }
+            
+            else if(s[i]==')'){
+                sum*=st.top();
+                st.pop();
+                sum+=st.top();
+                st.pop();
+            }
+            
+            else if(s[i]=='-'){
+                sign*=-1;
+            }
+        }
+        
+        return sum;
+    }
+};
